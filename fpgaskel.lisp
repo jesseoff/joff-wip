23074757527736b723721b33aa5b867f7d774542
Jesse Off <joff@embeddedarm.com>
2020-07-08 09:55:00 -0700

    Intern strings found in netlist

--- fpgaskel.lisp
+++ fpgaskel.lisp
@@ -23,15 +23,23 @@
 ;; *END*     OF ASCII OUTPUT FILE
 ;;
 ;; I get the feeling from google that there is no universal format for netlist files, but
-;; this seems to be Mentor Graphics PADS EDA software does in July, 2020.
+;; the above seems to be what Mentor Graphics PADS EDA software outputs in July, 2020, at least.
 ;;
-(defun pin-names (netlist-txt-pathname &key (designator "u8"))
+(defun pin-names (netlist-txt-pathname &key (designator "U8"))
   "Reads a PADS netlist.txt file and returns an alist of ((\"net-name\" . \"pin\") ... )"
-  (let ((part-pins-hash (make-hash-table :test 'equal)))
+  (let ((interned (make-hash-table :test 'equalp))
+        (part-pins-hash (make-hash-table)))
     (labels
-        ((sig-line (net-name &rest connections)
+        ((intern-strings (&rest strings)
+           (loop for s in strings
+                 for v = (gethash s interned)
+                 if (null v) do
+                   (setf (gethash s interned) s) and collect s
+                 else collect v))
+         (intern-string (s) (car (intern-strings s)))
+         (sig-line (net-name &rest connections)
            (loop for point in connections
-                 for (part pin) = (cl-ppcre:split "\\." point)
+                 for (part pin) = (apply #'intern-strings (cl-ppcre:split "\\." point))
                  do
                     (let ((pin-net-hash (gethash part part-pins-hash)))
                       (unless pin-net-hash
@@ -40,10 +48,10 @@
                       (setf (gethash pin pin-net-hash) net-name))))
          (nets-alist (part-designator)
            "Generates an alist of (net-name . pin) given a part-designator (e.g. U8)"
-           (loop
-             with part = (gethash (string-downcase part-designator) part-pins-hash)
-             for pin being the hash-key of part
-             collect (cons (gethash pin part) pin)))
+           (let ((part (gethash (intern-string part-designator) part-pins-hash)))
+             (when part (loop
+                          for pin being the hash-key of part
+                          collect (cons (gethash pin part) pin)))))
          (shorted-nets (alist)
            "Returns any net-names of pins shorted together in a list"
            (loop for (net . pin) in alist
@@ -61,9 +69,9 @@
           with x
           for line = (read-line s nil)
           while line
-          for words = (cl-ppcre:split " +" (string-downcase line))
+          for words = (cl-ppcre:split " +" line)
           if (and words (char-equal (aref (car words) 0) #\*)) do
-            (when (and x (equal "*sig*" (car x))) (apply #'sig-line (cdr x)))
+            (when (and x (equalp "*SIG*" (car x))) (apply #'sig-line (cdr x)))
             (setf x words)
           else do
             (setf x (nconc x words))))
@@ -71,24 +79,20 @@
 
 (defun verilog (pin-names-alist &key (toplevel-module-name "XXX_top"))
   "Generates Verilog boilerplate."
-  (loop initially (format t "module ~a (~&  ~a_pad" toplevel-module-name (caar pin-names-alist))
+  (loop initially (format t "module ~a (~&  ~(~a~)_pad" toplevel-module-name (caar pin-names-alist))
         for (wire . pin) in (cdr pin-names-alist)
-        do (format t ",~&  ~a_pad" wire)
+        do (format t ",~&  ~(~a~)_pad" wire)
         finally (format t "~&);~&~&"))
   (loop for (wire . pin) in pin-names-alist
-        do (format t "inout ~a_pad; /* pin ~a */~&" wire pin)
+        do (format t "inout ~(~a~)_pad; /* pin ~a */~&" wire pin)
         finally (format t "endmodule")))
 
 (defun qsf (pin-names-alist)
   "Generates Quartus .qsf file boilerplate"
   (loop for (name . pin) in pin-names-alist
-        do (format t "set_location_assignment PIN_~@:(~a~) -to ~a_pad~&" pin name)))
+        do (format t "set_location_assignment PIN_~@:(~a~) -to ~(~a~)_pad~&" pin name)))
 
 (defun lpf (pin-names-alist)
   "Generate Lattice Diamond .lpf file boilerplate"
   (loop for (name . pin) in pin-names-alist
-        do (format t "LOCATE COMP \"~a_pad\" SITE \"~@:(~a~)\";~&" name pin)))
-
-
-
-
+        do (format t "LOCATE COMP \"~(~a~)_pad\" SITE \"~@:(~a~)\";~&" name pin)))
