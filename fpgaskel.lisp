6dfbf38f7b3b5cc91f9cb953a327165cdd96ecb0
Jesse Off <joff@embeddedarm.com>
2020-07-13 16:29:57 -0700

    Updates/Improvements

--- fpgaskel.lisp
+++ fpgaskel.lisp
@@ -25,6 +25,17 @@
 ;; I get the feeling from google that there is no universal format for netlist files, but
 ;; the above seems to be what Mentor Graphics PADS EDA software outputs in July, 2020, at least.
 ;;
+
+(defun net-name-sanitize (net)
+  "This translates net names to be Verilog designator safe. (remove #'s, periods, etc..)"
+  (setf net (cl-ppcre:regex-replace-all "([0-9])\\.([0-9])([^_]+)" net "\\1\\3\\2")
+        net (cl-ppcre:regex-replace-all "^([0-9][^_]+)_(.*)" net "\\2_\\1"))
+  (multiple-value-bind (padn match) (cl-ppcre:regex-replace-all "(.*)#(.*)" net "\\1\\2_padn")
+    (if match
+        (setf net padn)
+        (setf net (concatenate 'string net "_pad"))))
+  (string-downcase net))
+
 (defun fpga-pinlocks (netlist-asc-pathname &key designator)
   "Reads a PADS netlist.asc file and returns an alist of ((\"net-name\" . \"pin\") ... )"
   (let ((interned (make-hash-table :test 'equalp))
@@ -82,26 +93,62 @@
             (setf x words)
           else do
             (setf x (nconc x words))))
-      (values (no-shorted-nets
-               (nets-alist (if designator designator fpga-best-guess)))
-              designator-hash))))
+      (loop for (pad . pin) in (no-shorted-nets
+                                (nets-alist (if designator designator fpga-best-guess)))
+            collect (cons (net-name-sanitize pad) pin)))))
 
-(defun verilog (pin-names-alist &key (toplevel-module-name "XXX_top"))
+
+(defun verilog (pin-names-alist)
   "Generates Verilog boilerplate."
-  (loop initially (format t "module ~a (~&  ~(~a~)_pad" toplevel-module-name (caar pin-names-alist))
-        for (wire . pin) in (cdr pin-names-alist)
-        do (format t ",~&  ~(~a~)_pad" wire)
-        finally (format t "~&);~&~&"))
-  (loop for (wire . pin) in pin-names-alist
-        do (format t "inout ~(~a~)_pad; /* pin ~a */~&" wire pin)
-        finally (format t "endmodule")))
+  (flet
+      ((inout-pad-assign-statement (pad-name)
+         (let* ((p (cl-ppcre:split "_pad" pad-name))
+                (n (car p)))
+           (format nil "assign ~a = ~a_oe ? ~a~a : 'bz;" pad-name n (if (cdr p) "!" "") n)))
+       (core-instance-connections (pad-name)
+         (let* ((p (cl-ppcre:split "_pad" pad-name))
+                (n (car p)))
+           (format nil "  .~a_i(~a~a),~&  .~a_o(~a),~&  .~a_oe_o(~a_oe)"
+                   n (if (cdr p) "!" "") pad-name
+                   n n n n)))
+       (core-ports (pad-name)
+         (let* ((p (cl-ppcre:split "_pad" pad-name))
+                (n (car p)))
+           (format nil "  input ~a_i,~&  output ~a_o, ~a_oe_o" n n n))))
+    (let* ((nets (mapcar #'car pin-names-alist))
+           (top-ports (mapcar (lambda (x) (concatenate 'string "inout " x)) nets))
+           (assigns (mapcar #'inout-pad-assign-statement nets))
+           (core-ports (mapcar #'core-ports nets))
+           (core-instance (mapcar #'core-instance-connections nets)))
+      (format t "module top(~&~{  ~a~^,~&~}~&);~&~{~a~&~}" top-ports assigns)
+      (format t "core core_inst(~&~{~a~^,~&~});~&endmodule~&" core-instance)
+      (format t "module core(~&~{~a~^,~&~});~&`include \"core.v\"~&endmodule" core-ports))))
 
 (defun qsf (pin-names-alist)
   "Generates Quartus .qsf file boilerplate"
+  (format t "set_global_assignment -name FAMILY \"Cyclone IV GX\"
+set_global_assignment -name DEVICE EP4CGX30CF19C8
+set_global_assignment -name TOP_LEVEL_ENTITY top
+set_global_assignment -name ORIGINAL_QUARTUS_VERSION 18.1.0
+set_global_assignment -name PROJECT_CREATION_TIME_DATE \"09:54:01  JULY 09, 2020\"
+set_global_assignment -name LAST_QUARTUS_VERSION \"18.1.0 Lite Edition\"
+set_global_assignment -name PROJECT_OUTPUT_DIRECTORY output_files
+set_global_assignment -name MIN_CORE_JUNCTION_TEMP 0
+set_global_assignment -name MAX_CORE_JUNCTION_TEMP 85
+set_global_assignment -name DEVICE_FILTER_PACKAGE FBGA
+set_global_assignment -name DEVICE_FILTER_PIN_COUNT 324
+set_global_assignment -name ERROR_CHECK_FREQUENCY_DIVISOR 1
+set_global_assignment -name NOMINAL_CORE_SUPPLY_VOLTAGE 1.2V
+set_global_assignment -name VERILOG_FILE top.v~&")
   (loop for (name . pin) in pin-names-alist
-        do (format t "set_location_assignment PIN_~@:(~a~) -to ~(~a~)_pad~&" pin name)))
+        do (format t "set_location_assignment PIN_~@:(~a~) -to ~a~&" pin name)))
+
+(defun qpf ()
+  (format t "QUARTUS_VERSION = \"18.1\"
+DATE = \"09:54:01  July 09, 2020\"
+PROJECT_REVISION = \"top\""))
 
 (defun lpf (pin-names-alist)
   "Generate Lattice Diamond .lpf file boilerplate"
   (loop for (name . pin) in pin-names-alist
-        do (format t "LOCATE COMP \"~(~a~)_pad\" SITE \"~@:(~a~)\";~&" name pin)))
+        do (format t "LOCATE COMP \"~a\" SITE \"~@:(~a~)\";~&" name pin)))
